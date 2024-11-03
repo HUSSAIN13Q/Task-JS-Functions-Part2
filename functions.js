21800309ce503e7d60dbf43ebbef5808126ef4d9
HUSSAIN13Q <hussainalsaffar20@gmail.com>
2024-11-03 13:40:29 +0300

    task 2 done

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -21,6 +21,11 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -33,7 +38,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +52,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 != 0) {
+    return n * n;
+  } else {
+    return n * 2;
+  }
 }
 
 module.exports = {
